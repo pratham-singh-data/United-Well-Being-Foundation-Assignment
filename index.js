c6f06a5d46e3c458b5b77dca278f0642d6a7e0a2
pratham-singh-data <PrathamSinghCE@gmail.com>
2023-01-19 13:43:39 +0530

    Navbar Done

---
+++ index.js
@@ -0,0 +1,29 @@
+const adjustNavbar = () => {
+    const checkInput = document.getElementById("check");
+    const navList = document.getElementById("navlist");
+    
+    if(checkInput.checked){
+        // if navlist is to be shown
+        navList.className = "hidden";
+    }
+    else{
+        // if navlist is not to be shown
+        navList.className = "";
+    }
+}
+
+const setPage = () => {
+    const location = window.location.href;
+    const currentLocation = location.slice(location.lastIndexOf("/") + 1, location.length);
+    const currentItem = document.getElementById(currentLocation ? currentLocation : "home");
+
+    currentItem.className = "currentPage";
+}
+
+const loadFuctionalities = () => {
+    // set navbar
+    adjustNavbar();
+
+    // set active page
+    setPage();
+}
